f5ea81c002cafe02daa1270fdd1123631b63acc3
ktatu <kaikkonentatu@gmail.com>
2025-01-19 21:41:50 +0200

    task 1

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,2 +1,35 @@
+import re
+
+# this still allows things like test- to pass as identifiers
 def tokenize(source_code: str) -> list[str]:
-    return ["test"]
+    whitespace_re = re.compile(r"\s+")
+    # maybe \b shouldnt be at the start? might break something later
+    identifier_re = re.compile(r"\b[A-Za-z_][A-Za-z_0-9]*")
+    integer_literal_re = re.compile(r"[0-9]+")
+
+    tokens: list[str] = []
+    position = 0
+
+    while position < len(source_code):
+        whitespace_match = whitespace_re.match(source_code, position)
+        identifier_match = identifier_re.match(source_code, position)
+        integer_literal_match = integer_literal_re.match(source_code, position)
+
+
+        if whitespace_match is not None:
+            position = whitespace_match.end()
+            continue
+
+        if identifier_match is not None:
+            tokens.append(source_code[position:identifier_match.end()])
+            position = identifier_match.end()
+            continue
+
+        if integer_literal_match is not None:
+            tokens.append(source_code[position:integer_literal_match.end()])
+            position = integer_literal_match.end()
+            continue
+
+        raise Exception("No match found, position: ", position, ", source code at position: ", source_code[position])
+
+    return tokens
--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -1,4 +1,22 @@
 from compiler.tokenizer import tokenize
 
+### WHITESPACE ###
 def test_tokenizer_skips_whitespace() -> None:
-    assert tokenize("test") == ["test"]
+    assert tokenize("   \n  \n ") == []
+### ###
+
+### IDENTIFIERS ###
+def test_tokenizer_recognizes_an_identifier() -> None:
+    assert tokenize(" test ") == ["test"]
+
+def test_tokenizer_recognizes_multiple_identifiers() -> None:
+    assert tokenize("if var_name \n while") == ["if", "var_name", "while"]
+### ###
+
+### INTEGER_LITERALS ###
+def test_tokenizer_recognizes_integer_literal() -> None:
+    assert tokenize("123") == ["123"]
+
+def test_tokenizer_recognizes_multiple_integer_literals() -> None:
+    assert tokenize(" 123 555") == ["123", "555"]
+### ###
