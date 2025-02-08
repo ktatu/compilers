35c75006865b4f3e89c9ca343d63fc5bb3d47bed
ktatu <kaikkonentatu@gmail.com>
2025-02-09 00:18:38 +0200

    Interpreter supports identifiers

--- src/compiler/interpreter.py
+++ src/compiler/interpreter.py
@@ -13,43 +13,55 @@ class SymTab:
 
 # mistä tiedetään, millä hierarkisella tasolla kullakin hetkellä ollaan?
 def interpret(node: ast.Expression, sym_tab: SymTab = None) -> Value:
-    def add_symbol(node: ast.VariableDeclaration):
-        value = interpret(node.initialize)
+    # jokainen rekursio on uusi taso sym_tablessa
+    # kun mennään uuteen tasoon, luodaan uusi sym_tab
+    # parametrina saadaan edellisten tasojen ketju
+    current_tab = SymTab({}, sym_tab)
 
-        if sym_tab is not None:
-            sym_tab[node.name] = value
-        else:
-            locals_dict = {node.name: value}
-            sym_tab = SymTab(locals_dict)
+    def add_symbol(node: ast.VariableDeclaration):
+        value = interpret(node.initialize, current_tab)
+        current_tab.locals[node.name] = value
 
     match node:
         case ast.Literal():
             return node.value
 
-        # tässä asetetaan sym_tabiin arvo joka määritetään
-        # var y = 5; var x = y; pitäisi toimia, y pitää lukea symbol tablesta
-        # tässä on kylläkin block
         case ast.VariableDeclaration():
             add_symbol(node)
             return None
 
-        # case { var x = 5; x }, x on result
-        # oletetaan, että tämä on ylin taso: ei voida vaan suoraan hypätä resulttiin
-        # pitää varmistaa, että VariableDeclaration käsitellään jossain vaiheessa
-        # x:n pitää löytyä joko saman tason blockista tai jostakin parent-tablesta
+        case ast.Identifier():
+            table = sym_tab
+            identifier = None
+
+            try:
+                while node.name not in table.locals.keys():
+                    table = sym_tab.parent
+                identifier = table.locals[node.name]
+            except:
+                raise Exception(
+                    f"{node.location}: could not find value for identifier {node.name}"
+                )
+
+            return identifier
+
         case ast.Block():
             for expr in node.statements:
                 if isinstance(expr, ast.VariableDeclaration):
                     add_symbol(expr)
 
-            if node.result.value is None:
+            if isinstance(node.result, ast.Literal) and node.result.value is None:
                 return None
             else:
-                return interpret(node.result, sym_tab)
+                # jos on Identifier, niin pitäisi aina löytyä sym_tablesta
+                if isinstance(expr, ast.Identifier):
+                    print("asd")
+                else:
+                    return interpret(node.result, current_tab)
 
         case ast.BinaryOp():
-            a: Any = interpret(node.left, sym_tab)
-            b: Any = interpret(node.right, sym_tab)
+            a: Any = interpret(node.left, current_tab)
+            b: Any = interpret(node.right, current_tab)
             if node.op == "+":
                 return a + b
             elif node.op == "<":
@@ -59,6 +71,6 @@ def interpret(node: ast.Expression, sym_tab: SymTab = None) -> Value:
 
         case ast.Conditional():
             if interpret(node.cond_if):
-                return interpret(node.then, sym_tab)
+                return interpret(node.then, current_tab)
             else:
-                return interpret(node.cond_else, sym_tab)
+                return interpret(node.cond_else, current_tab)
--- tests/interpreter_test.py
+++ tests/interpreter_test.py
@@ -2,6 +2,7 @@ from tests.tokenizer_test import L
 from compiler.tokenizer import tokenize
 from compiler.parser import parse
 from compiler.interpreter import Value, interpret
+import pytest
 
 
 def test_interpret(source_code: str) -> Value:
@@ -25,3 +26,15 @@ def test_interpreter_handles_block_with_no_result() -> None:
 
 def test_interpreter_handles_block_with_result() -> None:
     assert test_interpret(f"{{ 1 }}") == 1
+
+
+### BLOCK + IDENTIFIER ###
+def test_interpreter_handles_identifier_declared_on_same_level() -> None:
+    assert test_interpret("var x = 5; x") == 5
+
+
+def test_interpreter_throws_exception_when_it_cannot_find_identifier() -> None:
+    with pytest.raises(Exception) as e:
+        test_interpret("var x = 5; y")
+
+    assert str(e.value) == f"{L}: could not find value for identifier y"
